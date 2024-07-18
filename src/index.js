28fa16238048964d189e5c8dfef6b9d846cb1270
martinscovenant <martinscovenant1@gmail.com>
2024-07-18 09:59:00 +0100

    cartcontextprovider export

--- src/index.js
+++ src/index.js
@@ -6,27 +6,32 @@ import App from './App'
 // Authentication
 import { Register, Login } from './components'
 // import { AuthProvider } from './Context/AuthContext'
-import PrivateRoute from './Utils/PrivateRoute'
+// import PrivateRoute from './Utils/PrivateRoute'
+import { CartProvider } from './Pages/ItemDetails/ItemDetails'
 
 // pages
 import { TopDeals, PopularProducts, NewArrivals, Item, Cart } from './Pages'
+
 import { render } from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 render(
   <BrowserRouter>
     {/* <AuthProvider> */}
-      <Routes>
-        <Route path='/' element={<App />}></Route>
-        <Route path='/top-deals' element={<TopDeals />} />
-        <Route path='/popular-products' element={<PopularProducts />} />
-        <Route path='/new-arrivals' element={<NewArrivals />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/Cart' element={<Cart />} />
-        {/* item details */}
-        <Route path='/product/detail/:idl_product_code/:supplier_id' element={<Item />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path='/' element={<App />}></Route>
+          <Route path='/top-deals' element={<TopDeals />} />
+          <Route path='/popular-products' element={<PopularProducts />} />
+          <Route path='/new-arrivals' element={<NewArrivals />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/Cart' element={<Cart />} />
+
+          {/* item details */}
+          <Route path='/product/detail/:idl_product_code/:supplier_id' element={<Item />} />
+        </Routes>
+      </CartProvider>
     {/* </AuthProvider> */}
   </BrowserRouter>,
   document.getElementById('root')
